24e86aa6eace13641553c87ebf2798aa0e491801
daisyy0000 <tpfksghd95@gmail.com>
2023-02-21 23:06:41 +0900

    [230221 화] Studyroom, hostuser, roominfo 엔티티 매핑관계 및 단위테스트 완료 (+fav_study, reservation도 테스트하다가 같이 추가)

--- back/src/main/java/com/developer/hostuser/entity/HostUser.java
+++ back/src/main/java/com/developer/hostuser/entity/HostUser.java
@@ -1,9 +1,21 @@
 package com.developer.hostuser.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import com.developer.reservation.entity.Reservation;
+import com.developer.studyroom.entity.Studyroom;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,27 +29,40 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "host_user")
-public class HostUser {
+
+@DynamicInsert()  //ColumnDefault때문에
+@DynamicUpdate()
+public class HostUser { 
 	
 	@Id
 	@Column(name="host_id")
 	private String hostId;
 	
-	@Column(name="pwd")
+	@Column(name="pwd", nullable = false)
 	private String pwd;
 	
-	@Column(name="num")
+	@Column(name="num", nullable = false)
 	private String num;
 	
 	@Column(name="ready")
+	@ColumnDefault(value = "0")
 	private Integer ready;
 	
-	@Column(name="name")
+	@Column(name="name" , nullable = false)
 	private String name;
 	
-	@Column(name="tel")
+	@Column(name="tel", nullable = false)
 	private String tel;
 	
-	@Column(name="email")
+	@Column(name="email", nullable = false)
 	private String email;
+	
+	
+	
+	@OneToOne(mappedBy = "hostUser")
+	private Studyroom studyroom;
+	
+	
+	@OneToMany(mappedBy = "hostUser")
+	private List<Reservation> reservation;
 }
