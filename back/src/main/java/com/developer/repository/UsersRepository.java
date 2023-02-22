392b2e9e34cbad9ec32bfe5959338e8c9120457f
nowij0 <moonone470@gmail.com>
2023-02-22 22:27:24 +0900

    지원파트 repository

--- back/src/main/java/com/developer/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.developer.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.developer.users.entity.Users;
-
-public interface UsersRepository extends CrudRepository<Users, String> {
-
-}
---
+++ back/src/main/java/com/developer/users/repository/UsersRepository.java
@@ -0,0 +1,16 @@
+package com.developer.users.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.developer.users.entity.Users;
+
+public interface UsersRepository extends JpaRepository<Users, String> {
+	
+	//TUTOR + USERS 동시 출력 
+	@EntityGraph(attributePaths = "tutor")
+	public Optional<Users> findByUserId(String userId);
+ 
+}
