4326828efddad0d85a84eaa72a1a90ed8b79568a
daisyy0000 <tpfksghd95@gmail.com>
2023-02-23 16:22:51 +0900

    지원 병합충돌2

--- back/src/main/java/com/developer/users/entity/Users.java
+++ back/src/main/java/com/developer/users/entity/Users.java
@@ -7,16 +7,16 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
+import com.developer.appliedlesson.entity.AppliedLesson;
 import com.developer.board.entity.Board;
 import com.developer.boardrep.entity.BoardRep;
 import com.developer.recommend.entity.Recommend;
-import com.developer.appliedlesson.entity.AppliedLesson;
 import com.developer.tutor.entity.Tutor;
 
 import lombok.AllArgsConstructor;
@@ -36,36 +36,6 @@ public class Users{
 	@Column(name = "user_id")
 	private String userId;
 	
-	@Column(name = "role")
-	@ColumnDefault(value="0")
-	private int role;
-	
-	@Column(name = "pwd")
-	private String pwd;
-	
-	@Column(name = "nickname")
-	private String nickname;
-	
-	@Column(name = "name")
-	private String name;
-	
-	@Column(name = "email")
-	private String email;
-	
-	@Column(name = "tel")
-	private String tel;
-	
-	@Column(name = "addr")
-	private String addr;
-	
-	@OneToMany(cascade = CascadeType.REMOVE, mappedBy="users")
-	private List<Board> board;
-	
-	@OneToMany(cascade = CascadeType.REMOVE, mappedBy="users")
-	private List<BoardRep> boardRep;
-	
-	@OneToMany(cascade = CascadeType.REMOVE, mappedBy="users")
-	private List<Recommend> recommend;
 	@Column(name="role", columnDefinition = "NUMBER DEFAULT 2")
 	private Integer role;
 	@Column(name="pwd", nullable = false)
@@ -81,12 +51,21 @@ public class Users{
 	@Column(name="addr", nullable = false)
 	private String addr;
 	
+	@OneToMany(cascade = CascadeType.REMOVE, mappedBy="users")
+	private List<Board> board;
 	
-	@OneToOne(mappedBy = "users",
+	@OneToMany(cascade = CascadeType.REMOVE, mappedBy="users")
+	private List<BoardRep> boardRep;
+	
+	@OneToMany(cascade = CascadeType.REMOVE, mappedBy="users")
+	private List<Recommend> recommend;
+	
+    @OneToOne(mappedBy = "users",
 			cascade = CascadeType.REMOVE)
 	private Tutor tutor;
 	
 	@OneToMany(mappedBy = "user")
 	private List<AppliedLesson> appliedLesson;
+	
 	}
 
