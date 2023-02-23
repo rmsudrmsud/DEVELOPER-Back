258e3b5d1d98a490b9594d624dfa91c26b928a22
daisyy0000 <tpfksghd95@gmail.com>
2023-02-23 15:54:44 +0900

    근형님꺼병합충돌 2

--- back/src/main/java/com/developer/boardrep/entity/BoardRep.java
+++ back/src/main/java/com/developer/boardrep/entity/BoardRep.java
@@ -1,46 +1,5 @@
 package com.developer.boardrep.entity;
 
-import java.sql.Date;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Entity
-@Table(name = "board_rep")
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-
-public class BoardRep {
-	@Id
-	@Column(name = "post_req_seq")
-	private Long postRepSeq;
-
-	@Column(name = "content")
-	private String content;
-
-	@Column(name = "cdate")
-	private Date cDate;
-
-	@Column(name = "post_seq")
-	private Integer postSeq;
-
-	@Column(name = "user_id")
-	private String userId;
-}
-package com.developer.boardrep.entity;
-
-
-
 import java.util.Date;
 
 import javax.persistence.Column;
