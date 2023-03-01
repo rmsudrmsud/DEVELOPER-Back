5aa9a01c868c28b20dfab9d9febba01a61d5c128
daisyy0000 <tpfksghd95@gmail.com>
2023-03-01 16:56:17 +0900

    깃 재설정(캐시제거후)

--- back/src/test/java/com/developer/LessonReview/LessonReviewRepositoryTest.java
+++ back/src/test/java/com/developer/LessonReview/LessonReviewRepositoryTest.java
@@ -1,36 +1,36 @@
-package com.developer.lessonreview;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-import java.util.Optional;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.developer.appliedlesson.entity.AppliedLesson;
-import com.developer.appliedlesson.repository.AppliedLessonRepository;
-import com.developer.lessonreview.entity.LessonReview;
-import com.developer.lessonreview.repository.LessonReviewRepository;
-
-@SpringBootTest
-public class LessonReviewRepositoryTest {
-	private Logger logger = LoggerFactory.getLogger(getClass()); 
-	@Autowired
-	private LessonReviewRepository lrRepository;
-	@Autowired
-	private AppliedLessonRepository alRepository;
-	
-	@Test
-	void test(){
-		
-	}
-	
+//package com.developer.lessonreview;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import com.developer.appliedlesson.entity.AppliedLesson;
+//import com.developer.appliedlesson.repository.AppliedLessonRepository;
+//import com.developer.lessonreview.entity.LessonReview;
+//import com.developer.lessonreview.repository.LessonReviewRepository;
+//
+//@SpringBootTest
+//public class LessonReviewRepositoryTest {
+//	private Logger logger = LoggerFactory.getLogger(getClass()); 
+//	@Autowired
+//	private LessonReviewRepository lrRepository;
+//	@Autowired
+//	private AppliedLessonRepository alRepository;
+//	
+//	@Test
+//	void test(){
+//		
+//	}
+//	
 //	@Test
 //	@DisplayName("수업후기 INSERT 테스트")
 //	void testSave() {
@@ -62,14 +62,14 @@ public class LessonReviewRepositoryTest {
 //		assertEquals(expected, optLR.get().getApplySeq());
 //	}
 //	
-	@Test
-	@DisplayName("수업후기 SELECT 테스트")
-	void testSelectDetail() {
-		Optional<LessonReview> optLR = lrRepository.findById(1L);
-		assertTrue(optLR.isPresent());
-		Long expected = 1L;
-		//assertEquals(expected, optLR.get().getApplySeq());
-	}
+//	@Test
+//	@DisplayName("수업후기 SELECT 테스트")
+//	void testSelectDetail() {
+//		Optional<LessonReview> optLR = lrRepository.findById(1L);
+//		assertTrue(optLR.isPresent());
+//		Long expected = 1L;
+//		//assertEquals(expected, optLR.get().getApplySeq());
+//	}
 	
 //	@Test
 //	@DisplayName("수업후기 SELECT 테스트: 작성한 후기 목록")
@@ -98,4 +98,4 @@ public class LessonReviewRepositoryTest {
 	
 
 
-}
+//}
