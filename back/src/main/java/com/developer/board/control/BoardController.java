e8222184b983f9a79a924f0f0c6b21e91ff0c914
rmsudrmsud <sky950214@naver.com>
2023-03-01 17:24:46 +0900

    세란님꺼 최종병합전 커밋

--- back/src/main/java/com/developer/board/control/BoardController.java
+++ back/src/main/java/com/developer/board/control/BoardController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.developer.board.dto.BoardDTO;
+import com.developer.board.entity.Board;
 import com.developer.board.service.BoardService;
 import com.developer.exception.AddException;
 import com.developer.exception.FindException;
@@ -47,10 +48,46 @@ public class BoardController {
 		bService.addBoard(saveBoardDTO, logined);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
-	/** [메인페이지] 글작성 최신순으로 list를 출력한다.
-	 * @author SR
-	 * @return ResponseEntity<?>
+	
+	
+	/**
+	 * 글 번호로 게시글 상세 검색(닉네임+글상세+댓글)
+	 * 
+	 * @author choigeunhyeong
+	 * @param postSeq
+	 * @return
 	 * @throws FindException
+	 */
+	@GetMapping(value = "select/{postSeq}" // , produces = MediaType.APPLICATION_JSON_VALUE
+			)
+			public ResponseEntity<?> selectAllPostSeq(@PathVariable Long postSeq) throws FindException {
+		List<BoardDTO.BoardAllSelectDTO> list= bService.selectAllPostSeq(postSeq);
+			return new ResponseEntity<>(list, HttpStatus.OK);
+				
+			}
+	
+	/**
+	 * 게시글 제목으로 검색
+	 * @author choigeunhyeong
+	 * @param title
+	 * @return
+	 * @throws FindException
+	 */
+	@GetMapping(value = "search/{title}" // , produces = MediaType.APPLICATION_JSON_VALUE
+			)
+			public ResponseEntity<?> searchBoard(@PathVariable String title) throws FindException{
+			List<Board> list= bService.findByTitle("%"+title+"%");
+			return new ResponseEntity<>(list, HttpStatus.OK);
+				
+			}
+	
+	/**
+	 * 글 수정폼.. 
+	 * @author choigeunhyeong
+	 * @param postSeq
+	 * @return
+	 * @throws FindException
+	 * 미완
 	 */
 	@GetMapping(value = "detail/{postSeq}" // , produces = MediaType.APPLICATION_JSON_VALUE
 			)
