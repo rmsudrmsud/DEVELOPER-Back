6cf9f1f64a900ac018f1b271345dd8b4a9543697
daisyy0000 <tpfksghd95@gmail.com>
2023-03-13 19:47:13 +0900

    [SR] 최종 백

--- back/src/main/java/com/developer/lesson/repository/LessonRepository.java
+++ back/src/main/java/com/developer/lesson/repository/LessonRepository.java
@@ -173,12 +173,12 @@ public interface LessonRepository extends JpaRepository<Lesson, Long> {
 		
 		//[SR]메인페이지 - 신청종료일 임박순으로 list 출력
 		@Query(value = "SELECT *"
-				+ "FROM (SELECT lesson_seq, lesson_name, img_path, price"
-				+ "                FROM lesson"
-				+ "                WHERE pay_lesson != 2"
-				+ "                AND TO_DATE(end_date, 'YY-MM-DD') >= TO_DATE(sysdate, 'YY-MM-DD')"
-				+ "                ORDER BY end_date ASC)"
-				+ "WHERE rownum BETWEEN 1 AND 6", nativeQuery = true)
+				+ "FROM (SELECT lesson_seq, lesson_name, img_path, price "
+				+ "                FROM lesson "
+				+ "                WHERE pay_lesson between 0 and 1 "
+				+ "                AND TO_DATE(end_date, 'YY-MM-DD') >= TO_DATE(sysdate, 'YY-MM-DD') "
+				+ "                ORDER BY end_date ASC) "
+				+ "WHERE rownum BETWEEN 1 AND 6 ", nativeQuery = true)
 		public List<Object[]> selectAllBydateLesson();
 		
 		//[DS]
--- back/src/main/java/com/developer/util/FileDownloadController.java
+++ back/src/main/java/com/developer/util/FileDownloadController.java
@@ -21,59 +21,57 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("download/*")
 public class FileDownloadController {
 
-	//근형
-		@GetMapping("board")
-		public ResponseEntity<?> download(String imgPath, int type, String opt) throws FindException {
+	// 근형
+	@GetMapping("board")
+	public ResponseEntity<?> download(String imgPath, int type, String opt) throws FindException {
 
-			String saveDirectory = "/Users/choigeunhyeong/Documents/attach";
+		String saveDirectory = "/Users/choigeunhyeong/Documents/attach";
 
-			String fileName = "";
-			if (type == 2) {
-				fileName = "t_";
-			}
-			fileName += imgPath; 
-			File dir = new File(saveDirectory); // 첨부파일이 있는 디렉토리
-			File file = null;
+		String fileName = "";
+		if (type == 2) {
+			fileName = "t_";
+		}
+		fileName += imgPath;
+		File dir = new File(saveDirectory); // 첨부파일이 있는 디렉토리
+		File file = null;
 
-			for (File f : dir.listFiles()) { // 디렉토리의 모든 파일들
+		for (File f : dir.listFiles()) { // 디렉토리의 모든 파일들
 
-				String fn = f.getName();
-				
-				if (fn.equals(fileName)) {
-					file = f;
-					fileName = f.getName();
-					break;
-				}
-			}
-			if (file == null) {
-				throw new FindException(fileName + "으로 된 파일이 없습니다");
-			}
-			try {
-			    HttpHeaders headers = new HttpHeaders();
-			    String contentType = Files.probeContentType(file.toPath());
-			    headers.add(HttpHeaders.CONTENT_TYPE, contentType);
-			    headers.add(HttpHeaders.CONTENT_LENGTH, "" + file.length());
+			String fn = f.getName();
 
-			    if ("attachment".equals(opt)) {
-			        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename*=UTF-8''"
-			                + URLEncoder.encode(file.getName(), "UTF-8").replace("+", "%20"));
-			    } else {
-			        headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline;filename*=UTF-8''"
-			                + URLEncoder.encode(file.getName(), "UTF-8").replace("+", "%20"));
-			    }
-
-			    byte[] bArr = FileCopyUtils.copyToByteArray(file);
-			    ResponseEntity<?> re = new ResponseEntity<>(bArr, headers, HttpStatus.OK);
-			    System.out.println(bArr);
-			    return re;
-			} catch (Exception e) {
-			    throw new FindException(e.getMessage());
+			if (fn.equals(fileName)) {
+				file = f;
+				fileName = f.getName();
+				break;
 			}
 		}
-	
+		if (file == null) {
+			throw new FindException(fileName + "으로 된 파일이 없습니다");
+		}
+		try {
+			HttpHeaders headers = new HttpHeaders();
+			String contentType = Files.probeContentType(file.toPath());
+			headers.add(HttpHeaders.CONTENT_TYPE, contentType);
+			headers.add(HttpHeaders.CONTENT_LENGTH, "" + file.length());
+
+			if ("attachment".equals(opt)) {
+				headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename*=UTF-8''"
+						+ URLEncoder.encode(file.getName(), "UTF-8").replace("+", "%20"));
+			} else {
+				headers.add(HttpHeaders.CONTENT_DISPOSITION,
+						"inline;filename*=UTF-8''" + URLEncoder.encode(file.getName(), "UTF-8").replace("+", "%20"));
+			}
+
+			byte[] bArr = FileCopyUtils.copyToByteArray(file);
+			ResponseEntity<?> re = new ResponseEntity<>(bArr, headers, HttpStatus.OK);
+			System.out.println(bArr);
+			return re;
+		} catch (Exception e) {
+			throw new FindException(e.getMessage());
+		}
+	}
+
 	/**
-	 * 수업 파일 다운로드
-	 * 
 	 * @author SR
 	 * @param imgPath
 	 * @param type    첨부파일인 경우 1, 섬네일파일인 경우 2
@@ -90,7 +88,6 @@ public class FileDownloadController {
 		if (type == 2) {
 			fileName = "t_";
 		}
-		fileName += imgPath; 
 		fileName += imgPath;
 		File dir = new File(saveDirectory); // 첨부파일이 있는 디렉토리
 		File file = null;
@@ -98,8 +95,6 @@ public class FileDownloadController {
 		for (File f : dir.listFiles()) { // 디렉토리의 모든 파일들
 
 			String fn = f.getName();
-			//int lastIndex = fn.lastIndexOf(".");
-			//if (fn.substring(0, lastIndex).equals(fileName)) {
 			// int lastIndex = fn.lastIndexOf(".");
 			// if (fn.substring(0, lastIndex).equals(fileName)) {
 			if (fn.equals(fileName)) {
@@ -128,7 +123,6 @@ public class FileDownloadController {
 
 			byte[] bArr = FileCopyUtils.copyToByteArray(file);
 			ResponseEntity<?> re = new ResponseEntity<>(bArr, headers, HttpStatus.OK);
-			System.out.println(bArr);
 			return re;
 		} catch (Exception e) {
 			throw new FindException(e.getMessage());
